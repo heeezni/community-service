fe82a514af6c4fff46be52ed9629a29768b28a03
heeezni <hjin3456@naver.com>
2025-09-24 13:03:45 +0900

    🩹 Simple Fix : 코드 포매터(Spotless) 적용

--- src/test/java/com/example/communityservice/CommunityServiceApplicationTests.java
+++ src/test/java/com/example/communityservice/CommunityServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommunityServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
